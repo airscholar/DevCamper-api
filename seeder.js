05c4cdac0174dbc7f3ad73735a0029bb804b3720
Yusuf Ganiyu <airscholar@gmail.com>
2020-12-13 17:25:00 +0100

    Updated seeder to add courses add part of the seeding

--- seeder.js
+++ seeder.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config({ path: './config/config.env' });
 
 //load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 const { nextTick } = require('process');
 
 //connect to dB
@@ -16,11 +17,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 //read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
 
 // import into dB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
+        await Course.create(courses)
         console.log(`Data imported`.green.inverse)
     } catch (err) {
         // next(err)
@@ -31,7 +34,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
-
+        await Course.deleteMany()
         console.log(`Data destroyed`.red.inverse)
         process.exit(1 )
     } catch (err) {
