86c1a97415885bfee65f5194655d6c3617f0bc29
Yusuf Ganiyu <airscholar@gmail.com>
2020-12-22 18:58:20 +0100

    Added create course controller and route

--- controllers/courses/courses.controller.js
+++ controllers/courses/courses.controller.js
@@ -30,8 +30,8 @@ const getAllCourses = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc      Get all courses
-// @route     GET /api/v1/courses
+// @desc      Get single courses
+// @route     GET /api/v1/course/:id
 // @access    Public
 const getSingleCourse = asyncHandler(async (req, res, next) => {
   const courses = await Course.findById(req.params.id).populate({
@@ -52,7 +52,21 @@ const getSingleCourse = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc      Add a new course
+// @route     POST /api/v1/courses
+// @access    Public
+const addCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    message: 'Course created successfully',
+    data: course,
+  });
+});
+
 module.exports = {
   getAllCourses,
   getSingleCourse,
+  addCourse,
 };
--- routers/courses/courses.router.js
+++ routers/courses/courses.router.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const coursesController = require('../../controllers/courses/courses.controller');
 
-router.route('/').get(coursesController.getAllCourses);
+router
+  .route('/')
+  .get(coursesController.getAllCourses)
+  .post(coursesController.addCourse);
 router.route('/:id').get(coursesController.getSingleCourse);
 
 module.exports = router;
