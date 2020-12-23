9bccc0fe1328269a8b577ab37d03d492ef3c8ce1
Yusuf Ganiyu <airscholar@gmail.com>
2020-12-23 20:34:52 +0100

    Removed average cost from mock seed. Changing it to automatic calculation

--- seeder.js
+++ seeder.js
@@ -23,7 +23,7 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'u
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
-        await Course.create(courses)
+        // await Course.create(courses)
         console.log(`Data imported`.green.inverse)
     } catch (err) {
         // next(err)
@@ -34,7 +34,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
-        await Course.deleteMany()
+        // await Course.deleteMany()
         console.log(`Data destroyed`.red.inverse)
         process.exit(1 )
     } catch (err) {
