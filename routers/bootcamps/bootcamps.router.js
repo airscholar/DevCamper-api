5a6e784a25fce66b0766529c2808cb675dfd8269
Yusuf Ganiyu <airscholar@gmail.com>
2020-12-25 23:51:59 +0100

    Refactored bootcamp router

--- routers/bootcamps/bootcamps.router.js
+++ routers/bootcamps/bootcamps.router.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const bootcampController = require('../../controllers/bootcamps/bootcamps.controller');
+const {
+  getBootcampInRadius,
+  getAllBootcamps,
+  createNewBootcamp,
+  getBootcampById,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../../controllers/bootcamps/bootcamps.controller');
 const courseRouter = require('../courses/courses.router');
 
 //reroute other courses routes to courses
 router.use('/:bootcampId/courses', courseRouter);
 
-router
-  .route('/radius/:zipcode/:distance')
-  .get(bootcampController.getBootcampInRadius);
+router.route('/radius/:zipcode/:distance').get(getBootcampInRadius);
 
-router
-  .route('/')
-  .get(bootcampController.getAllBootcamps)
-  .post(bootcampController.createNewBootcamp);
+router.route('/').get(getAllBootcamps).post(createNewBootcamp);
 router
   .route('/:id')
-  .get(bootcampController.getBootcampById)
-  .put(bootcampController.updateBootcamp)
-  .delete(bootcampController.deleteBootcamp);
+  .get(getBootcampById)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
