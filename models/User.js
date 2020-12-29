e5d116d27dc03818550cbf09fa956297a69bede3
Yusuf Ganiyu <airscholar@gmail.com>
2020-12-29 22:03:32 +0100

    Added jsonwebtoken and bcrypt packages. Added User Model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter an email address'],
+    unique: true,
+    match: [
+      /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/,
+      'Please add a valid email address',
+    ],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'publisher'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a valid password'],
+    select: false,
+    minlength: 6,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
