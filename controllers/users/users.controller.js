ed455ad67d14a9f4c2b6939e2af83322671cf6dc
Yusuf Ganiyu <airscholar@gmail.com>
2020-12-31 20:51:17 +0100

    Updated Register User to POST

--- controllers/users/users.controller.js
+++ controllers/users/users.controller.js
@@ -3,7 +3,7 @@ const User = require('../../models/User');
 const asyncHandler = require('../../middleware/asyncHandler.middleware');
 
 // @desc      Regiser User
-// @route     GET /api/v1/auth/register
+// @route     POST /api/v1/auth/register
 // @access    Public
 const registerUser = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
