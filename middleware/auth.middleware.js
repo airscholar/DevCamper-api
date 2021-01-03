5a9775bb9b7df07ad72e8ee89f9c835203a2171f
Yusuf Ganiyu <airscholar@gmail.com>
2021-01-03 22:46:04 +0100

    Completed auth middleware to ensure users are logged in to access protected route

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const errorResponse = require('./error.middleware');
 const asyncHandler = require('./asyncHandler.middleware');
 const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse.helper');
 
 // protect route
 const protectRoute = asyncHandler(async (req, res, next) => {
@@ -15,7 +16,29 @@ const protectRoute = asyncHandler(async (req, res, next) => {
     token = req.headers.authorization.split(' ')[1];
   }
   // check token in cookies
-  else if (req.cookies.token) {
-    token = req.cookies.token;
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
+
+  if (!token) {
+    return next(
+      new ErrorResponse('Access to this section is not authorized!', 401)
+    );
+  }
+
+  try {
+    //  verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+
+    req.user = User.findById(decoded.id);
+
+    next();
+  } catch (err) {
+    return next(
+      new ErrorResponse('Access to this section is not authorized!', 401)
+    );
   }
 });
+
+module.exports = { protectRoute };
