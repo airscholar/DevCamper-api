6ebbd43290c608c20259a3ef6090cd942a018160
Yusuf Ganiyu <airscholar@gmail.com>
2021-04-16 14:58:30 +0100

    Updated users route

--- routers/users/users.route.js
+++ routers/users/users.route.js
@@ -9,24 +9,17 @@ const {
 const advancedResults = require('../../middleware/advancedResults.middleware');
 const {
   protectRoute,
-  
+
   authorize,
 } = require('../../middleware/auth.middleware');
 const User = require('../../models/User');
 
 const router = express.Router();
 
-router.use(protectRoute)
-router.use(authorize('admin', 'super admin'))
+router.use(protectRoute);
+router.use(authorize('admin', 'super admin'));
 
-router
-  .route('/')
-  .get(advancedResults(User), getUsers)
-  .post(createUser)
-  router
-  .route('/:userId')
-  .put(updateUser)
-  .get(getUser)
-  .delete(deleteUser);
-  
+router.route('/').get(advancedResults(User), getUsers).post(createUser);
+router.route('/:userId').put(updateUser).get(getUser).delete(deleteUser);
+
 module.exports = router;
